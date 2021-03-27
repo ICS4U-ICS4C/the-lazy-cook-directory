550587612e98a912a060283eab2d4962ae0121e2
783236 <54897132+783236@users.noreply.github.com>
2021-03-27 14:13:54 -0400

    some changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -25,7 +25,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffebee',
     alignItems: 'center',
     justifyContent: 'center',
   },
