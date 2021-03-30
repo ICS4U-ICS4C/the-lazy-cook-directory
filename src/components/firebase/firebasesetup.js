858d569c1ceedf6ca6811d3bd4cc3d7b6d1eb281
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2021-03-30 13:41:22 -0400

    master

---
+++ src/components/firebase/firebasesetup.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+//import "firebase/database";
+
+// // Initialize Firebase
+// const firebaseConfig = {
+//     apiKey: 'api-key',
+//     authDomain: 'project-id.firebaseapp.com',
+//     databaseURL: 'https://project-id.firebaseio.com',
+//     projectId: 'project-id',
+//     storageBucket: 'project-id.appspot.com',
+//     messagingSenderId: 'sender-id',
+//     appId: 'app-id',
+//     measurementId: 'G-measurement-id',
+//   };
+  
+//   firebase.initializeApp(firebaseConfig);
+
+/*^^ we need that to intialize firebase
+ in order to use database */
