337196c5ac95e860e30298a51852b721c32d5838
783236 <54897132+783236@users.noreply.github.com>
2021-04-01 19:31:20 -0400

    recipe style import

--- src/screens/Recipes.js
+++ src/screens/Recipes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 export default function Recipes(){
         return(
@@ -8,3 +8,14 @@ export default function Recipes(){
             </View>
         ) 
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      backgroundColor: '#e0e0e0',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+    })
