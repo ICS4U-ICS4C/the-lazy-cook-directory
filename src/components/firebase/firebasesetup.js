f8daff617cc140b01e5d9613d63fe3993c1d1dd3
696499 <54900211+696499@users.noreply.github.com>
2021-04-02 17:19:52 -0400

    properly connecting firebase

--- src/components/firebase/firebasesetup.js
+++ src/components/firebase/firebasesetup.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
-import "firebase/database";
+import 'firebase/database';
+import 'firebase/firestore';
 //all the firebase services
 //import "firebase/functions";
 //import "firebase/storage";
@@ -7,26 +8,20 @@ import "firebase/database";
 //import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgtQGskekbiYPKRxrm2BMYHMZhgv96-kc",
-    authDomain: "lazycookdirectory.firebaseapp.com",
-    databaseURL: "https://lazycookdirectory-default-rtdb.firebaseio.com",
-    projectId: "lazycookdirectory",
-    storageBucket: "lazycookdirectory.appspot.com",
-    messagingSenderId: "468616258749",
-    appId: "1:468616258749:web:05ba278d87a9a4db9403d4",
-    measurementId: "G-YSP97ZTR45"
-  };
+  apiKey: "AIzaSyAgtQGskekbiYPKRxrm2BMYHMZhgv96-kc",
+  authDomain: "lazycookdirectory.firebaseapp.com",
+  databaseURL: "https://lazycookdirectory-default-rtdb.firebaseio.com",
+  projectId: "lazycookdirectory",
+  storageBucket: "lazycookdirectory.appspot.com",
+  messagingSenderId: "468616258749",
+  appId: "1:468616258749:web:05ba278d87a9a4db9403d4",
+  measurementId: "G-YSP97ZTR45"
+};
+//initializing app only if it hasnt already been initialized
+if(firebase.apps.length == 0) {
   firebase.initializeApp(firebaseConfig);
-
-  //database connection
-  const dbh = firebase.firestore();
-
-  export default dbh;
-
-
-
-
+}
 /*^^ we need that to intialize firebase
  in order to use database */
-
+export default firebaseConfig;
  //https://www.kaggle.com/kaggle/recipe-ingredients-dataset -> our dataset
