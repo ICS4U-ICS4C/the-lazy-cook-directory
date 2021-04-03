00142d9e9e6fae714cab4aea0a1fe5b6717e51e6
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2021-04-02 23:12:33 -0400

    Delete Navigator.js

--- src/navigations/Navigator.js
+++
@@ -1,5 +0,0 @@
-// import HomeScreen from '../screens/HomeComponents/Home';
-// import Recipes from '../screens/Recipes';
-// import searchResults from '../screens/sResults';
-// import IngredientScreen from '../screens/Ingredients';
-// import SearchScreen from '../screens/Search';
