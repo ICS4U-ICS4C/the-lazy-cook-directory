d73c94e3421be70e7dacb7279de4438b578150ee
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2021-04-04 02:05:15 -0400

    integrating firebase and firestore

---
+++ database/firestore.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import "firebase/firestore";
+
+// this file is to connect our code to the firestore database
+const configuration = {
+    apiKey: "AIzaSyAgtQGskekbiYPKRxrm2BMYHMZhgv96-kc",
+    authDomain: "lazycookdirectory.firebaseapp.com",
+    databaseURL: "https://lazycookdirectory-default-rtdb.firebaseio.com",
+    projectId: "lazycookdirectory",
+    storageBucket: "lazycookdirectory.appspot.com",
+    messagingSenderId: "468616258749",
+    appId: "1:468616258749:web:20ec6ce9a815e3dd9403d4",
+    measurementId: "G-Q0BJ5NF3WX"
+}
+
+firebase.initializeApp(configuration);
+
+const db = firebase.firestore(); // database
+export default db;
