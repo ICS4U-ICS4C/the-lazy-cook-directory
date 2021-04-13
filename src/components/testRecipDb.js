95a6eead53b3ae42a2fbbdb306a3445279b5f035
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2021-04-13 16:53:43 -0400

    flatlist

---
+++ src/components/testRecipDb.js
@@ -0,0 +1,24 @@
+export default
+    {
+        name: 'Sweet and Sour Noodles',
+        ingredients:{
+            noodles:'noodles',
+            garlic:'garlic',
+            corn_starch:'corn starch',
+            soy_sauce:'soy sauce',
+            honey: 'honey',
+            lemon:'lemon',
+            water:'water'
+        },
+        quntity:{
+            noodQuant:'125g',
+            garQuanr:'1 Clove'
+
+        },
+        prep:{
+            stepOne: 'blah blah',
+            stepTwo:'blnfduihreiu'
+        },
+        duration: '17min',
+
+    }
