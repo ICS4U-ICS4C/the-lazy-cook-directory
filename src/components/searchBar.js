d5253e021176562b4d20d2a71ab7208ae911967d
696499 <54900211+696499@users.noreply.github.com>
2021-04-14 19:53:44 -0400

    changed the buttons and alert for no matching recipe
    anyone can change styling for buttons to how they want

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,6 @@
 import { Navigation } from '@material-ui/icons';
 import React, {Component, useEffect, useState} from 'react';
-import {View, TextInput, StyleSheet, StatusBar, FlatList, Text, Button, ScrollView,Alert} from 'react-native';
+import {View, TextInput, StyleSheet, StatusBar, FlatList, Text, Button, ScrollView,Alert,Pressable} from 'react-native';
 import theRecipes from '../db/firebaseConfig';
 import IngredientItem from './ingredientItem';
 import {listt} from './testRecipDb';
@@ -36,13 +36,13 @@ export default function SearchBar({navigation}){
     }
     //keeping track of what user types in a string
     const [text,setText] = useState('');
-    //when called takes in value that user typed
+    //when called takes in value that user typed and sets it to that value
     const changeHandler = (val) =>{
         setText(val);
     }
     //takes in text to update state/ingredient and adds new ingredient to screen
     const submit = (text) =>{
-        //checks if something is written
+        //checks if an ingredient is written, if it isnt then an alert pops up
         if(text.length >1){
         setingredients((priorIngredients) =>{
             return [
@@ -57,7 +57,6 @@ export default function SearchBar({navigation}){
     }
     //for searching ingredients, how to access each ingredient. store this
     //value into another array which we will use to search
-
     const search = (ingredients) =>{
       
         let newArray = []
@@ -72,13 +71,18 @@ export default function SearchBar({navigation}){
         for (let i =0; i< newArray.length; i++){
             let col = firestore.collection('Recipes').where('ingredients','array-contains', newArray[i]).get()
                 .then(snapshot=>{
+                    if(snapshot.empty){
+                        Alert.alert("Sorry no matching recipes")
+                        {text: 'ok'}
+                    }
                     snapshot.docs.forEach(doc =>{
-                        console.log(doc.id,doc.data())
+                        console.log(doc.data().name)
                     })
                 })
-
+                .catch(err =>{
+                    console.log("error:", err);
+                });
         }
-
     }
     return(
         <View style={styles.container}>
@@ -86,12 +90,19 @@ export default function SearchBar({navigation}){
                 
                 <TextInput style={styles.searchInput} placeholder = "Insert Ingredients here... "
                     onChangeText={changeHandler}/>
-                <Button onPress={()=> submit(text)} title='Add' color = 'green'/> 
-                <Button title = 'Search' color = 'green' onPress = {()=> search(ingredients)} /> 
 
+                     <Pressable style={styles.button} onPress={()=> submit(text)}>
+                        <Text style={styles.text}>Add</Text>
+                    </Pressable>
+
+                    <Pressable style={styles.buttonSearch}  onPress = {()=> search(ingredients)}>
+                        <Text style={styles.textSearch}>Search</Text>
+                    </Pressable>
+
+    
                 <View style = {styles.list}>
                 <FlatList
-                    //horizontal = {true}
+                    horizontal = {true}
                     data = {ingredients}
                     renderItem ={({item}) => (
                         <IngredientItem item = {item} pressDelete ={pressDelete}/>
@@ -122,7 +133,7 @@ export default function SearchBar({navigation}){
 
 //     },[])
 // }
-
+//added temporary styling for buttons, anyone can change them to how they like
 const styles = StyleSheet.create({
     container:{
         marginTop: StatusBar.currentHeight,
@@ -139,6 +150,42 @@ const styles = StyleSheet.create({
         color:'#2e2e2e'
     },
     list:{
+        //flex: 1,
         marginTop: 15
-    }
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        backgroundColor: '#8cbb6c',
+        borderStyle: 'dashed',
+        borderColor: 'black',
+        marginBottom: 10,
+      },
+      text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+      },
+      buttonSearch:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        backgroundColor: '#8cbb6c',
+        borderStyle: 'dashed',
+        borderColor: 'black',
+      },
+      textSearch:{
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+      }
 })
