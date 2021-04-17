e131099bd7045a08f45b41bcd03b08d1bf6446e4
696499 <54900211+696499@users.noreply.github.com>
2021-04-16 23:36:19 -0400

    fixed text error

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -45,15 +45,15 @@ const FEATURED = [
    ],
  }
 ];
-//<Navigation/>
 export default () => {
   return (
     <View style={styles.container}>
       <Card style={styles.introCard}>
-      <Card.Content> {/**this card contains the contructions and
-       * search bar, one could think of this as a div function
-       * in HTML
-       */}
+      <Card.Content> 
+           {/* *this card contains the contructions and
+//        * search bar, one could think of this as a div function
+//        * in HTML
+//        */}
         <Title>Welcome to the Lazy Cooks' Directory</Title>
         <Paragraph>Input the ingredients you want to cook with and we'll give you a recipe you thought was never possible with minimal ingredients!</Paragraph>
         <Paragraph> Note: for seasonings aside from salt and paper, please write "seasoning", the type will be specified in the recipe.</Paragraph>
