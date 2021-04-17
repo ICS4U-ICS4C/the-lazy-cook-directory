c8073de64ae42bc144f6eea7fd7679c318430bc9
696499 <54900211+696499@users.noreply.github.com>
2021-04-17 18:25:56 -0400

    button

--- src/components/category.js
+++ src/components/category.js
@@ -4,17 +4,24 @@ import {
     Text,
     StyleSheet,
     Image,
-    Button } from "react-native";
+    Button,
+    TouchableOpacity } from "react-native";
 
 class Category extends Component {
     render() {
         return (
             <View style={{ height: 130, width: 130, borderWidth: 0.5, borderColor: '#dddddd' }}>
                 <View style={{ flex: 2 }}>
-                    <Button icon={this.props.imageUri}
+
+                        <TouchableOpacity onPress={ () => this.props.link()} 
+                        style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}>
+                            <Image source= {require('../../src/icons/BLTSandwich.png')} />
+                        </TouchableOpacity>
+                        
+                    {/* <Button icon={this.props.imageUri}
                         style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
                         onPress={ () => this.props.link()}
-                    > </Button>
+                    /> */}
                 </View>
                 <View style={{ flex: 1, paddingTop: 10 }}>
                     <Text>{this.props.name}</Text>
